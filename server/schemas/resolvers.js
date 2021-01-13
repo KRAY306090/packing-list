7b04c923cc11a40d70aa9f7baed79b509f333e83
Kennadee <kennadee.ray@gmail.com>
2021-01-12 21:33:54 -0600

    apollo server setup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,3 +17,5 @@ const resolvers = {
         }
     }
 }
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,6 +13,15 @@ type User {
     email: String
     password: String
 }
+
+type Query {
+    users: [User]
+    user(username: String!): User
+}
+
+type Mutation {
+    addUser(username: String, email: String, password: String): User
+}
 `
 
 module.exports = typeDefs;
