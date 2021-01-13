768090c3dcbaea3cdb527b2f5c7fd56883e47c9a
Kennadee <kennadee.ray@gmail.com>
2021-01-12 21:25:27 -0600

    resolvers created

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,18 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+input UserInput {
+    username: String
+    email: String
+    password: String
+}
 
-
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+}
 `
 
 module.exports = typeDefs;
